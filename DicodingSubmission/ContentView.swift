fb74a295816e999a19d8d02d6bf80980ef6a2d6f
Dion Arya Pamungkas <dionarya.p@gmail.com>
2024-01-04 12:46:28 +0700

    Initial Commit

---
+++ DicodingSubmission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DicodingSubmission
+//
+//  Created by Dion Arya Pamungkas on 04/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DicodingSubmission/DicodingSubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicodingSubmissionApp.swift
+//  DicodingSubmission
+//
+//  Created by Dion Arya Pamungkas on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DicodingSubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
